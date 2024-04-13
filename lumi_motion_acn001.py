0679f4f0139b9475d6ba97a842b7bcedc4824ee6
LQV <luuquangvu89@gmail.com>
2024-04-13 20:06:55 +0700

    Update draft for lumi.motion.acn001

--- lumi_motion_acn001.py
+++ lumi_motion_acn001.py
@@ -4,7 +4,13 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-from zhaquirks import Bus
+from zigpy import types
+from zigpy.profiles import zha
+from zigpy.quirks import CustomDevice
+from zigpy.zcl.clusters.general import Basic, Identify, Ota, PowerConfiguration
+from zigpy.zcl.clusters.measurement import IlluminanceMeasurement, OccupancySensing
+
+from zhaquirks import Bus, LocalDataCluster
 from zhaquirks.const import (
     DEVICE_TYPE,
     ENDPOINTS,
@@ -14,21 +20,28 @@ from zhaquirks.const import (
     PROFILE_ID,
 )
 from zhaquirks.xiaomi import (
+    LocalIlluminanceMeasurementCluster,
     MotionCluster,
+    OccupancyCluster,
     XiaomiAqaraE1Cluster,
     XiaomiPowerConfiguration,
 )
-from zigpy.profiles import zha
-from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Basic, Identify, Ota, PowerConfiguration
-from zigpy.zcl.clusters.measurement import IlluminanceMeasurement, OccupancySensing
 
 MOTION_ATTRIBUTE = 274
+DETECTION_INTERVAL = 0x0102
+MOTION_SENSITIVITY = 0x010C
+TRIGGER_INDICATOR = 0x0152
 _LOGGER = logging.getLogger(__name__)
 
 
 class OppleCluster(XiaomiAqaraE1Cluster):
     """Opple cluster."""
+
+    attributes = {
+        DETECTION_INTERVAL: ("detection_interval", types.uint8_t, True),
+        MOTION_SENSITIVITY: ("motion_sensitivity", types.uint8_t, True),
+        TRIGGER_INDICATOR: ("trigger_indicator", types.uint8_t, True),
+    }
 
     def _update_attribute(self, attrid: int, value: Any) -> None:
         super()._update_attribute(attrid, value)
@@ -42,11 +55,43 @@ class OppleCluster(XiaomiAqaraE1Cluster):
                 OccupancySensing.Occupancy.Occupied,
             )
 
+    async def write_attributes(
+            self, attributes: dict[str | int, Any], manufacturer: int | None = None
+    ) -> list:
+        """Write attributes to device with internal 'attributes' validation."""
+        result = await super().write_attributes(attributes, manufacturer)
+        interval = attributes.get(
+            "detection_interval", attributes.get(DETECTION_INTERVAL)
+        )
+        _LOGGER.debug("detection interval: %s", interval)
+        if interval is not None:
+            self.endpoint.ias_zone.reset_s = int(interval)
+        return result
+
+
+class IlluminanceMeasurementClusterP1(LocalIlluminanceMeasurementCluster):
+    """Local illuminance measurement cluster that also discards more invalid values sent by this device."""
+
+    def _update_attribute(self, attrid, value):
+        if attrid == self.AttributeDefs.measured_value.id and (
+                value < 0 or value > 0xFFDC
+        ):
+            self.debug(
+                "Received invalid illuminance value: %s - setting illuminance to 0",
+                value,
+            )
+            value = 0
+        super()._update_attribute(attrid, value)
+
+
+class LocalOccupancyCluster(LocalDataCluster, OccupancyCluster):
+    """Local occupancy cluster."""
+
 
 class LocalMotionCluster(MotionCluster):
     """Local motion cluster."""
 
-    reset_s: int = 30
+    reset_s: int = 60
 
 
 class LumiMotionACN001(CustomDevice):
@@ -64,7 +109,7 @@ class LumiMotionACN001(CustomDevice):
         ENDPOINTS: {
             1: {
                 PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+                DEVICE_TYPE: zha.DeviceType.OCCUPANCY_SENSOR,
                 INPUT_CLUSTERS: [
                     Basic.cluster_id,
                     PowerConfiguration.cluster_id,
@@ -83,12 +128,14 @@ class LumiMotionACN001(CustomDevice):
         ENDPOINTS: {
             1: {
                 PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+                DEVICE_TYPE: zha.DeviceType.OCCUPANCY_SENSOR,
                 INPUT_CLUSTERS: [
                     Basic.cluster_id,
                     XiaomiPowerConfiguration,
                     Identify.cluster_id,
+                    LocalOccupancyCluster,
                     LocalMotionCluster,
+                    IlluminanceMeasurementClusterP1,
                     OppleCluster,
                 ],
                 OUTPUT_CLUSTERS: [
